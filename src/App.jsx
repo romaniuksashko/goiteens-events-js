205b66ea4ad7e93d8ac1837f965001fa905eaf8c
Oleksandr Romaniuk <romanuksashko@gmail.com>
2026-04-26 11:32:27 +0300

    All done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
+
 import './App.css'
 
 import data from "./upcoming-events.json"
 
+import { GlobalStyle } from './GlobalStyle'
 import { Section } from './Components/Section/Section'
 import { Title } from './Components/Title/Title'
 import { PageBoard } from './Components/PageBoard/PageBoard'
-
 
 
 function App() {
 
   return (
     <>
+      <GlobalStyle/>
       <Section>
         <Title />
         <PageBoard events={data} />
--- src/Components/PageBoard/PageBoard.style.js
+++ src/Components/PageBoard/PageBoard.style.js
@@ -4,7 +4,6 @@ export const List = styled.ul`
   background-color: #E2E6EA;
   border: #A2A8AD 5px solid;
   padding: 30px;
-  list-style: none;
   display: flex;
   flex-wrap: wrap;
   gap: 30px;
